e0f665774c2e52b8058e9f6ab804a04cb282c62c
Fawere54 <Fawere54@yandex.ru>
2026-01-03 21:39:01 +0300

    Version 0.1

--- game.py
+++ game.py
@@ -6,15 +6,16 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_TITLE = "Сигма-орбита"
 PLAYER_SPEED = 7  # Скорость движения игрока
+game = True
 
 
 class Bullet(arcade.Sprite):
     def __init__(self, filename, scale, speed):
         super().__init__(filename, scale)
-        self.change_y = speed  # Движение вверх
+        self.change_y = speed
 
     def update(self):
-        """Обновление позиции пули"""
+        # Обновление лазера
         self.center_y += self.change_y
 
         # Удаляем пулю, если она вышла за верхнюю границу экрана
@@ -28,23 +29,30 @@ class Enemy(arcade.Sprite):
         self.speed = speed
 
     def update(self):
+        # Обновление врага
         self.center_y -= self.speed
-        if self.bottom < -100:
-            self.remove_from_sprite_lists()
 
 
 class MyGame(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-        self.bg_sprite = arcade.Sprite("files/galaxy.jpg", scale=1.0)
-        self.bg_sprite.center_x = SCREEN_WIDTH // 2
-        self.bg_sprite.center_y = SCREEN_HEIGHT // 2
-        self.bg_sprite.width = SCREEN_WIDTH
-        self.bg_sprite.height = SCREEN_HEIGHT
+        # Создаем фон
+        self.bg_game1 = arcade.Sprite("files/bg_space.jpg", scale=1.0)
+        self.bg_game1.center_x = SCREEN_WIDTH // 2
+        self.bg_game1.center_y = 700
+        self.bg_game1.width = SCREEN_WIDTH
+        self.bg_game1.height = 1400
+
+        self.bg_game2 = arcade.Sprite("files/bg_space.jpg", scale=1.0)
+        self.bg_game2.center_x = SCREEN_WIDTH // 2
+        self.bg_game2.center_y = 700
+        self.bg_game2.width = SCREEN_WIDTH
+        self.bg_game2.height = 1400
 
         # Создаем список для всех спрайтов
         self.all_sprites = arcade.SpriteList()
-        self.all_sprites.append(self.bg_sprite)
+        self.all_sprites.append(self.bg_game1)
+        self.all_sprites.append(self.bg_game2)
 
         # Создаем спрайт игрока
         self.player_sprite = arcade.Sprite("files/Player.png", scale=0.5)
@@ -55,75 +63,92 @@ class MyGame(arcade.Window):
         self.player_sprites = arcade.SpriteList()
         self.player_sprites.append(self.player_sprite)
 
+        # Создаем списки лазеров и врагов
         self.bullets_list = arcade.SpriteList()
-
         self.enemy_list = arcade.SpriteList()
 
-        # Инициализируем флаги управления
+        # Создаем флаги
         self.left_pressed = False
         self.right_pressed = False
         self.up_pressed = False
         self.down_pressed = False
+        self.game = True
 
+        # Создаем 3 врага
+        self.create_enemy()
+        self.create_enemy()
         self.create_enemy()
 
     def setup(self):
-        """Создание и размещение монет"""
-        # Здесь можно добавить создание монет или других объектов
         pass
 
     def on_draw(self):
-        """Отрисовка всех спрайтов"""
+        # Отрисовка всех спрайтов
         self.clear()
         self.all_sprites.draw()
         self.player_sprites.draw()
         self.bullets_list.draw()
         self.enemy_list.draw()
+        if not self.game:
+            arcade.draw_text("Game Over!", 310, 290, (255, 0, 0), 30)
 
     def on_update(self, delta_time):
-        """Обновляем состояние игры"""
-        # Обновляем позицию игрока в зависимости от нажатых клавиш
-        if self.left_pressed and not self.right_pressed:
-            self.player_sprite.center_x -= PLAYER_SPEED
-        elif self.right_pressed and not self.left_pressed:
-            self.player_sprite.center_x += PLAYER_SPEED
-        #
-        # if self.up_pressed and not self.down_pressed:
-        #     self.player_sprite.center_y += PLAYER_SPEED
-        # elif self.down_pressed and not self.up_pressed:
-        #     self.player_sprite.center_y -= PLAYER_SPEED
+        if self.game:
+            # Обновляем позицию игрока в зависимости от нажатых клавиш
+            if self.left_pressed and not self.right_pressed:
+                self.player_sprite.center_x -= PLAYER_SPEED
+            elif self.right_pressed and not self.left_pressed:
+                self.player_sprite.center_x += PLAYER_SPEED
 
-        # Ограничиваем движение игрока границами экрана
-        if self.player_sprite.left < 0:
-            self.player_sprite.left = 0
-        if self.player_sprite.right > SCREEN_WIDTH:
-            self.player_sprite.right = SCREEN_WIDTH
-        if self.player_sprite.bottom < 0:
-            self.player_sprite.bottom = 0
-        if self.player_sprite.top > SCREEN_HEIGHT:
-            self.player_sprite.top = SCREEN_HEIGHT
+            # Ограничиваем движение игрока границами экрана
+            if self.player_sprite.left < 0:
+                self.player_sprite.left = 0
+            if self.player_sprite.right > SCREEN_WIDTH:
+                self.player_sprite.right = SCREEN_WIDTH
 
-        for b in self.bullets_list:
-            b.update()
+            # Обновляем все лазеры
+            for b in self.bullets_list:
+                b.update()
 
-        for e in self.enemy_list:
-            e.update()
-
-        for bullet in self.bullets_list:
-            # Проверяем столкновение с каждым врагом
-            hit_list = arcade.check_for_collision_with_list(bullet, self.enemy_list)
-            if hit_list:
-                # Удаляем пулю при столкновении
-                bullet.remove_from_sprite_lists()
-
-                # Удаляем врагов, в которых попали (или наносим урон)
-                for enemy in hit_list:
-                    enemy.remove_from_sprite_lists()
-
+            # Обновляем всех врагов
+            for e in self.enemy_list:
+                e.update()
+                # При выходе за зону видимости создаем нового
+                if e.center_y < -100:
+                    e.remove_from_sprite_lists()
                     self.create_enemy()
 
+            # Проверяем есть ли столкновение пулей и врагом
+            for bullet in self.bullets_list:
+                hit_list = arcade.check_for_collision_with_list(bullet, self.enemy_list)
+                if hit_list:
+                    bullet.remove_from_sprite_lists()
+                    for enemy in hit_list:
+                        enemy.remove_from_sprite_lists()
+                        self.create_enemy()
+
+            for enemy in self.enemy_list:
+                hit_list = arcade.check_for_collision_with_list(enemy, self.player_sprites)
+                if hit_list:
+                    enemy.remove_from_sprite_lists()
+                    for player in hit_list:
+                        player.remove_from_sprite_lists()
+                        self.game = False
+
+            self.bg_game1.center_y -= 2
+            self.bg_game2.center_y -= 2
+
+            if self.bg_game1.center_y == -100:
+                self.bg_game2.center_y = 1300
+
+            if self.bg_game2.center_y == -100:
+                self.bg_game1.center_y = 1300
+        else:
+            arcade.draw_text("Game Over!", SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, (255, 0, 0), 15)
+
+
     def on_key_press(self, key, modifiers):
-        """Обработка нажатий клавиш для управления игроком"""
+        # Обработка нажатий клавиш для управления игроком
         if key in (arcade.key.LEFT, arcade.key.A):
             self.left_pressed = True
         elif key in (arcade.key.RIGHT, arcade.key.D):
@@ -134,7 +159,7 @@ class MyGame(arcade.Window):
             self.down_pressed = True
 
     def on_key_release(self, key, modifiers):
-        """Обработка отпускания клавиш"""
+        # Обработка отпускания клавиш
         if key in (arcade.key.LEFT, arcade.key.A):
             self.left_pressed = False
         elif key in (arcade.key.RIGHT, arcade.key.D):
@@ -145,6 +170,7 @@ class MyGame(arcade.Window):
             self.down_pressed = False
 
     def on_mouse_press(self, x, y, button, modifiers):
+        # Создаем лазер при ПКМ
         self.bullet = Bullet("files/laser.png", 0.3, 10)
         self.bullet.center_x = self.player_sprite.center_x
         self.bullet.center_y = self.player_sprite.center_y + self.player_sprite.height / 2
@@ -153,15 +179,16 @@ class MyGame(arcade.Window):
     def create_enemy(self):
         self.enemy = Enemy("files/enemyShip1.png", 0.5, random.randint(3, 5))
         self.enemy.center_x = random.randint(100, SCREEN_WIDTH-100)
-        self.enemy.center_y = 550
+        self.enemy.center_y = 650
         self.enemy_list.append(self.enemy)
 
 
-
 def main():
-    game = MyGame()
-    game.setup()
-    arcade.run()
+    global game
+    if game:
+        game = MyGame()
+        game.setup()
+        arcade.run()
 
 
 if __name__ == "__main__":
